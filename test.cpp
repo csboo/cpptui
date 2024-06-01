88656d534ae1b1332ccf6fe30534f5be84ff3a13
csboo <csongiboo@gmail.com>
2024-06-01 19:47:18 +0200

    using latest tui.hpp, small fix

--- test.cpp
+++ test.cpp
@@ -22,26 +22,24 @@ namespace tui {
         }
 
         // void add_text(const std::string& text, Align alignment = Align::TopLeft, int x = 0, int y = 0) {
-            // texts.push_back({text, alignment, x, y});
+        // texts.push_back({text, alignment, x, y});
         // }
 
         void init_box() {
             const Box_chars& chars = styles.at(style);
 
             // Top border
-            
+
             // Side borders
 
             // Bottom border
         };
-        void print(){
-            
-        }
+        void print() {}
 
-        inline int get_width() const {return this->width;}
-        inline void set_width(unsigned width) {this->width = width;}
-        inline int get_height() const {return this->width;}
-        inline void set_height(unsigned height) {this->height = height;}
+        inline int get_width() const { return this->width; }
+        inline void set_width(unsigned width) { this->width = width; }
+        inline int get_height() const { return this->width; }
+        inline void set_height(unsigned height) { this->height = height; }
 
       private:
         struct Box_chars {
@@ -57,7 +55,7 @@ namespace tui {
             int y;
         };
 
-        struct Coord{
+        struct Coord {
             int x;
             int y;
         };
@@ -69,18 +67,15 @@ namespace tui {
         Coord aend;
         Coord rstart;
         Coord rend;
-        
+
         std::string title;
 
         Style style;
         std::vector<Text> texts;
         const std::unordered_map<Style, Box_chars> styles = {
-            {Style::Light, {{"┌", "┐", "┘", "└"}, "─", "│"}},
-            {Style::Heavy,  {{"┏", "┓", "┛", "┗"}, "━", "┃"}},
-            {Style::Double, {{"╔", "╗", "╝", "╚"}, "═", "║"}},
-            {Style::Dashed, {{"┌", "┐", "┘", "└"}, "┄", "┆"}},
-            {Style::Empty, {{" ", " ", " ", " "}, " ", " "}},
-            {Style::Rounded, {{"╭", "╮", "╯", "╰"}, "─", "│"}},
+            {Style::Light, {{"┌", "┐", "┘", "└"}, "─", "│"}},  {Style::Heavy, {{"┏", "┓", "┛", "┗"}, "━", "┃"}},
+            {Style::Double, {{"╔", "╗", "╝", "╚"}, "═", "║"}}, {Style::Dashed, {{"┌", "┐", "┘", "└"}, "┄", "┆"}},
+            {Style::Empty, {{" ", " ", " ", " "}, " ", " "}},  {Style::Rounded, {{"╭", "╮", "╯", "╰"}, "─", "│"}},
         };
 
         // int get_align(Align alignment) const {
@@ -118,19 +113,33 @@ namespace tui {
     };
 } // namespace tui
 
+void handle_resize(int) {
+    tui::screen::clear();
+}
+
 int main() {
-    tui::init_term(true);
-    
-    int width = 20;
-    int height = 10;
-    tui::Box alma(width, height, tui::Box::Style::Rounded);
-    std::cerr << "main box init done\n";
-    alma.print();
-    
-    char car = '\0';
-    // tui::cursor::set_position();
-    std::cout << "q to quit";
-    while(car != 'q'){ std::cin.get(car); }
+    tui::init_term(false);
+    tui::set_up_resize(handle_resize);
+
+    try {
+        int width = 20;
+        int height = 10;
+        tui::Box alma(width, height, tui::Box::Style::Rounded);
+        std::cerr << "main box init done\n";
+        alma.print();
+
+        char car = '\0';
+        // tui::cursor::set_position();
+        std::cout << "q to quit";
+        while (car != 'q') {
+            std::cin.get(car);
+        }
+    } 
+    catch (...) {
+        tui::reset_term();
+        std::cout << "ran into a problem";
+        return 1;
+    }
     tui::reset_term();
     return 0;
 }
