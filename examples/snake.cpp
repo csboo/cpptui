c1650e4336d8f5cbbe90337efb2ea6e0381c0883
Jeromos Kovacs <iitsnotme214@proton.me>
2024-06-06 09:28:30 +0200

    something

--- examples/snake.cpp
+++ examples/snake.cpp
@@ -1,10 +1,13 @@
 #include "../tui.hpp"
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <string>
 #include <thread>
 #include <vector>
+
+std::ofstream logfile("log.log");
 
 struct Coord {
     unsigned row;
@@ -31,34 +34,6 @@ enum Direction {
     Left,
     Right,
 };
-
-using Snake = std::vector<Coord>;
-
-void move(Snake& snake, const Direction& dir) {
-    // delete the last one
-    auto last = snake.back();
-    tui::cursor::set_position(last.row, last.col);
-    std::cout.put(' ');
-
-    auto* head = &snake.front();
-
-    switch (dir) {
-    case Up:
-        head->row--;
-        break;
-    case Down:
-        head->row++;
-        break;
-    case Left:
-        head->col--;
-        break;
-    case Right:
-        head->col++;
-        break;
-    default:
-        break;
-    }
-}
 Direction from_char(const char& ch) {
     switch (ch) {
     case 'k':
@@ -71,9 +46,77 @@ Direction from_char(const char& ch) {
         return Direction::Left;
     default:
         // unreachable
-        ;
+        break;
     }
     // unreachable
+}
+Direction opposite(const Direction& dir) {
+    switch (dir) {
+    case Direction::Up:
+        return Direction::Down;
+    case Direction::Down:
+        return Direction::Up;
+    case Direction::Left:
+        return Direction::Right;
+    case Direction::Right:
+        return Direction::Left;
+    default:
+        break;
+    }
+}
+std::string to_string(const Direction& dir) {
+    switch (dir) {
+    case Direction::Up:
+        return "A";
+    case Direction::Down:
+        return "V";
+    case Direction::Left:
+        return "<";
+    case Direction::Right:
+        return ">";
+    }
+}
+
+using Snake = std::vector<Coord>;
+
+void handle_movement(const Direction& dir, Coord*& coord) {
+    switch (dir) {
+    case Up:
+        coord->row--;
+        break;
+    case Down:
+        coord->row++;
+        break;
+    case Left:
+        coord->col--;
+        break;
+    case Right:
+        coord->col++;
+        break;
+    default:
+        break;
+    }
+}
+void move(Snake& snake, const Direction& dir) {
+    Coord* tail = &snake.back();
+    Coord* head = &snake.front();
+
+    // delete the last one off the screen by overwriting it with a space
+    tui::cursor::set_position(tail->row, tail->col);
+    std::cout.put(' ');
+    // std::cout << "tail change!";
+    // snake.pop_back();
+    // snake.push_back(snake.back());
+    // tail = &snake.at(snake.size() - 2);
+    for (auto i = 1; i < snake.size(); ++i) {
+        logfile << "previous: " << snake.at(i - 1).display() << "\n";
+        logfile << "current: " << snake.at(i).display() << "\n";
+
+        snake.at(i) = snake.at(i - 1);
+    }
+    logfile << "\n";
+
+    handle_movement(dir, head);
 }
 
 void run() {
@@ -81,7 +124,7 @@ void run() {
     auto screen_size = Coord{screen_size_pair.first, screen_size_pair.second};
 
     char ch = 'l';
-    auto apple = Coord{1, 2};
+    auto apple = Coord::random();
     auto dir = Direction::Right;
     Snake snake = {Coord{1, 1}};
 
@@ -90,29 +133,42 @@ void run() {
         move(snake, dir);
         // snake ate apple, we need a new one!
         if (snake.front() == apple) {
-            snake.push_back(apple);
+            snake.push_back(Coord{snake.back().row - 1, apple.col - 1});
             tui::cursor::set_position(30, 0);
             std::cout << "--apple--";
+            logfile << "\nAPPLE\n";
             apple = Coord::random();
             tui::cursor::set_position(apple.row, apple.col);
-            std::cout.put('x');
+            std::cout << tui::tui_string('@').red().bold();
         } else {
             tui::cursor::set_position(30, 0);
-            std::cout << "--     --\r\n"
-                      << snake.front().display() << "\r\n"
-                      << snake.back().display() << "\r\n"
-                      << apple.display();
+            std::cout << "--     --\r\n";
+            auto i = 0;
+            for (auto item : snake) {
+                std::cout << i << ": " << item.display() << "           \r\n";
+                i++;
+            }
+            std::cout << "\r\napple: " << apple.display() << "          \r\nlen: " << snake.size();
         }
 
         for (const Coord& item : snake) {
             tui::cursor::set_position(item.row, item.col);
-            std::cout.put((item == snake.front() ? '@' : '#'));
+            tui::tui_string x;
+            if (item == snake.front()) {
+                x += to_string(dir);
+                x = x.cyan();
+            } else if (item == snake.back()) {
+                x = "&";
+            } else {
+                x = "-";
+            }
+            std::cout << x;
         }
 
         std::cin.get(ch);
+        logfile << "\'" << ch << "\'\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(250));
 }
 
 int main() {
