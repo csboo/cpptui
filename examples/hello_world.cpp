d9c03c7225f28be0e5f8e246e0b81edf723c0566
csboo <csongiboo@gmail.com>
2025-03-19 00:04:54 +0100

    misc: use more parentheses for math expressions

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -14,11 +14,11 @@ int main() {
     while (input != SpecKey::CtrlC && input != 'q') { // main loop
         // NOTE: use `.size()` before styling, as it'd be completely crazy after applying styles
         // `msg` will be in the middle of the screen
-        tui::cursor::set_position(screen_size.first / 2 - 1, screen_size.second / 2 - (msg.size() / 2));
+        tui::cursor::set_position((screen_size.first / 2) - 1, (screen_size.second / 2) - (msg.size() / 2));
         std::cout << msg.blue().link("https://github.com/csboo/cpptui").bold().underline();
 
         // `msg` will be in the middle of the screen
-        tui::cursor::set_position(screen_size.first / 2 + 1, screen_size.second / 2 - (note.size() / 2));
+        tui::cursor::set_position((screen_size.first / 2) + 1, (screen_size.second / 2) - (note.size() / 2));
         std::cout << note.on_rgb(106, 150, 137).rgb(148, 105, 117).italic().dim();
 
         std::cout.flush();
